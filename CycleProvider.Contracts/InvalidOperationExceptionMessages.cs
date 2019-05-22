3119d6cf830c5c2f6d31d5d0ff418561ee2246de
antinontoxic <skolodziej@o2.pl>
2019-05-22 12:19:55 +0200

    Final version

---
+++ CycleProvider.Contracts/InvalidOperationExceptionMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycleProvider.Contracts
+{
+    public static class InvalidOperationExceptionMessages
+    {
+        public static readonly string EmptyCycleProvider = "Cycle Provider has no items";
+    }
+}
