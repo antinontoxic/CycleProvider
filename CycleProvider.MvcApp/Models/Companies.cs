c325a3c9a15747bd76a6a370e100e39e78afd238
antinontoxic <skolodziej@o2.pl>
2019-05-24 14:49:28 +0200

    Final

---
+++ CycleProvider.MvcApp/Models/Companies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CycleProvider.MvcApp.Models
+{
+    public class Companies
+    {
+        public int Id { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Company without name cannot exist"), StringLength(25)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [DisplayName("Year of creation")]
+        public int CreationYear { get; set; }
+    }
+}
